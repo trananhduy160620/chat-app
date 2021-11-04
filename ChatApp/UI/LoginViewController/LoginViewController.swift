235cf04d95bdbb8f00c930fb2d359020e148bca0
trananhduy <tanhduy160620@gmail.com>
2021-11-04 14:44:04 +0700

    [chat-app] handle keyboard when input data

--- ChatApp/UI/LoginViewController/LoginViewController.swift
+++ ChatApp/UI/LoginViewController/LoginViewController.swift
@@ -14,10 +14,35 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
-    
+    @IBOutlet weak var bottomContainerViewAnchor: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLoginAndRegisterButton()
+        setupTextField()
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyBoard(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyBoard(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(handletextField))
+        view.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func handletextField() {
+        emailTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+    }
+    
+    @objc private func handleKeyBoard(notification: Notification) {
+        if let userInfo = notification.userInfo {
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+            let isKeyboardShowing = notification.name == UIResponder.keyboardWillShowNotification
+            bottomContainerViewAnchor.constant = isKeyboardShowing ? (keyboardFrame.height + 0) : 0
+            
+            UIView.animate(withDuration: 0, delay: 0, options: .curveEaseInOut, animations: {
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     private func transitionToHomeVC() {
@@ -25,6 +50,18 @@ class LoginViewController: UIViewController {
         let navBar = UINavigationController(rootViewController: homeVC)
         self.view.window?.rootViewController = navBar
         self.view.window?.makeKeyAndVisible()
+    }
+    
+    private func setupTextField() {
+        emailTextField.layer.cornerRadius = 5
+        emailTextField.layer.borderWidth = 1.5
+        emailTextField.layer.borderColor = UIColor.darkGray.cgColor
+        emailTextField.layer.masksToBounds = true
+        
+        passwordTextField.layer.cornerRadius = 5
+        passwordTextField.layer.borderWidth = 1.5
+        passwordTextField.layer.borderColor = UIColor.darkGray.cgColor
+        passwordTextField.layer.masksToBounds = true
     }
     
     private func setupLoginAndRegisterButton() {
